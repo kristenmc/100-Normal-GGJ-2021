8086ba9dbab7be21a8337f5be743732e0c12b75c
M3RLN <32886755+M3RLN@users.noreply.github.com>
2021-01-30 11:31:59 -0800

    Resource Bars now working with GameManager

--- 100 Normal GGJ 2021/Assets/Scripts/ResourceBar.cs
+++ 100 Normal GGJ 2021/Assets/Scripts/ResourceBar.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class ResourceBar : MonoBehaviour
 {
     [SerializeField] Image Foodbar;
-    [SerializeField] float resourceNum;
-    [SerializeField] float resourceMax;
+    float resourceNum;
+    float resourceMax;
+    [SerializeField] string resourceType;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,16 @@ public class ResourceBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (resourceType == "Food")
+        {
+            resourceNum = GameManager.Game_Manager_Instance.getFoodAmt();
+            resourceMax = GameManager.Game_Manager_Instance.getMaxFood();
+        }
+        if (resourceType == "Water")
+        {
+            resourceNum = GameManager.Game_Manager_Instance.getWaterAmt();
+            resourceMax = GameManager.Game_Manager_Instance.getMaxWater();
+        }
         Foodbar.fillAmount = resourceNum / resourceMax;
     }
 }
