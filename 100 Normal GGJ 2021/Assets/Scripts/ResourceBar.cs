367638814c7beb98ccb4c6c58717f311b84e94dd
M3RLN <32886755+M3RLN@users.noreply.github.com>
2021-01-29 21:55:58 -0800

    Pushing Resource Bar Prefab

---
+++ 100 Normal GGJ 2021/Assets/Scripts/ResourceBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceBar : MonoBehaviour
+{
+    [SerializeField] Image Foodbar;
+    [SerializeField] float resourceNum;
+    [SerializeField] float resourceMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Foodbar.fillAmount = resourceNum / resourceMax;
+    }
+}
