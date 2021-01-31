92c439a27c5ada0be45efa14ed997b28deb4fc9b
M3RLN <32886755+M3RLN@users.noreply.github.com>
2021-01-30 19:20:34 -0800

    RandomBeatMap generator is done

---
+++ 100 Normal GGJ 2021/Assets/Scripts/BeatMapMaker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatMapMaker : MonoBehaviour
+{
+    /*This is the class made to create beatMaps. For reference, a 0 implies no action should be taken on that particular half count, 
+     *a 1 implies that the player needs to take a game action (pressing a button), and a 2 implies end of the item that is terminated with a game action
+     * (for fishing, the time when the fish would be fully out of the water).*/
+
+    [SerializeField] string smallFish = "02";
+    [SerializeField] string BIGFish = "00101012";
+    [SerializeField] string medFish = "00102";
+
+    public List<string> endList;
+    [SerializeField] List<string> listOfThings;
+
+    private void Start()
+    {
+        listOfThings = new List<string>();
+        listOfThings.Add(smallFish);
+        listOfThings.Add(BIGFish);
+        listOfThings.Add(medFish);
+    }
+
+    public string GenerateBeatMap(int length)
+    {
+        endList = new List<string>();
+        string endString = "";
+        for (int i = 0; i < length; i++)
+        {
+            int randChoice = Random.Range(0, listOfThings.Count);
+            endString = endString + listOfThings[randChoice];
+        }
+
+
+        return endString;
+    }
+    
+
+
+}
