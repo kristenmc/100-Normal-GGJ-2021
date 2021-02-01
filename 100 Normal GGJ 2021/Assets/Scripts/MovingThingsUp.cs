9c0ca5f7682a4f38ee71cc363641b4061c53d8ed
M3RLN <32886755+M3RLN@users.noreply.github.com>
2021-01-31 17:30:37 -0800

    Moving Script Added

---
+++ 100 Normal GGJ 2021/Assets/Scripts/MovingThingsUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingThingsUp : MonoBehaviour
+{
+
+    [SerializeField] GameObject MoveableObject;
+    [SerializeField] float moveDistance;
+    [SerializeField] float moveTime;
+
+    void MoveTheObject()
+    {
+        LeanTween.moveY(MoveableObject, moveDistance, moveTime);
+        LeanTween.moveY(MoveableObject, -moveDistance, moveTime).setDelay(.1f);
+    }
+
+}
